e40f11abc5f7f95c389d69c94b7b88ed18235000
Tharaka Buddhi <tharaka.b@userdata.tech>
2025-01-23 10:51:39 +0800

    Minor fixes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="header">
-        <h1>Github Trending Repos</h1>
+        <h1>GitHub Trending Repos</h1>
         <h5>in last 10 days</h5>
       </header>
       <InfiniteScroller />
